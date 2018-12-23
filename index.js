e27644540f3249e28ca3a9ab0446cf1649c19e81
Piotr <ziolko.piotr@yahoo.pl>
2018-12-23 17:01:50 +0100

    :D

---
+++ index.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var request = require('request');
+
+app.use(bodyParser.json());
+
+app.set('port', (process.env.PORT || 5000));
+app.set('verify_token', (process.env.VERIFY_TOKEN || 'vbngfdsafghjgfdsadfbnhjdfs'));
+app.set('page_access_token', (process.env.PAGE_ACCESS_TOKEN || 'EAANCK6BfAIkBANxWqNjZBZBZBsTevfBpggn1KVyr4wxiDPWteeloNXnBZBKRLQyaeYmS5gU3WNhf0TZBqqQ9HkGjeESZBZC0O7ZBBw8YsXnkZAZBDbPlEayRd5LkFNLZAL7Hx2A9yvwkcYbMZAeyjhvKsLdZAlHDaWqw5ZBkyHJ7jmiZBaYJElPauy3K84b'));
+
+app.get('/', function (req, res) {
+        res.send('It Works! Follow FB Instructions to activate.');
+});
+
+app.get('/webhook', function (req, res) {
+    if (req.query['hub.verify_token'] === app.get('verify_token')) {
+        res.send(req.query['hub.challenge']);
+    } else {
+        res.send('Error, wrong validation token');
+    }
+});
+
+app.post('/webhook/', function (req, res) {
+    console.log (req.body);
+    messaging_events = req.body.entry[0].messaging;
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i];
+        sender = event.sender.id;
+        if (event.message && event.message.text) {
+            text = event.message.text;
+            // Your Logic Replaces the following Line
+            sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
+        }
+    }
+    res.sendStatus(200);
+});
+
+function sendTextMessage(sender, text) {
+    messageData = {
+        text:text
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:app.get('page_access_token')},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+}
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
